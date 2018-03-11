8f1523e0a06ee5a035ad74030d5b6b88098a3a03
Antoni Kowalski <antonik68@gmail.com>
2018-03-11 17:19:00 +0100

    added imag, and opening imae

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("siema")
+from PIL import Image
+
+image_dot = Image.open("/home/antq/PycharmProjects/TomographSimulator/Tomograph-simulator/test_images/dot.bmp")
+
+pixels = list(image_dot.getdata())
+print(pixels.__class__)
+for i in range (0,255):
+    for j in range (0,255):
+        print(pixels[j], end="")
+    print("\n")
