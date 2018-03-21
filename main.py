59f2e772d91e29b74af98442dfa34dd130900407
Antoni Kowalski <antonik68@gmail.com>
2018-03-21 12:03:27 +0100

    dziaua!

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import utils
 import math
 import numpy
 
+image_name = "centered_square.bmp"
 
-image = Image.open("test_images/centered_square.bmp")
+image = Image.open("test_images/" + image_name)
 pixels = image.load()
 
 number_of_steps = 360
@@ -12,16 +13,16 @@ scope = math.pi/3
 number_of_detectors = 80
 radius = image.size[0]//2
 
-# sinogram = utils.doTomography(scope, number_of_steps, number_of_detectors, radius, pixels, number_of_steps)
-#
+sinogram = utils.doTomography(scope, number_of_steps, number_of_detectors, radius, pixels, number_of_steps)
+
 # for i in range(0, number_of_steps):
 #     for j in range(0, number_of_detectors):
 #         print(sinogram[i * number_of_detectors + j][0], end=" ")
 #     print("\n")
-#
-# img = Image.new('RGB', (number_of_detectors, number_of_steps))
-# img.putdata(sinogram)
-# img.save("test_images/sinogram.bmp", "BMP")
+
+img = Image.new('RGB', (number_of_detectors, number_of_steps))
+img.putdata(sinogram)
+img.save("test_images/sinogram.bmp", "BMP")
 
 sinogram_image = Image.open("test_images/sinogram.bmp")
 sinogram_pixels = sinogram_image.load()
@@ -35,21 +36,18 @@ for sublist in out_list:
     for item in sublist:
         flat_list.append(item)
 
+normalized_list = []
 
 
 
+for element in flat_list:
+    normalized_element = element/max_value * 255
+    normalized_pixel = int(round(normalized_element)), int(round(normalized_element)), int(round(normalized_element))
+    normalized_list.append(normalized_pixel)
 
+img = Image.new('RGB', (radius * 2, radius * 2))
+img.putdata(normalized_list)
+img.save("out/" + image_name, "BMP")
 
-
-img = Image.new('RGB', (radius * 2, number_of_steps))
-
-
-
-
-
-# for i in range(0,number_of_detectors):
-#     for j in range(0, number_of_steps):
-#         print(sinogram[i * j], end=" ")
-#     print("\n")
 
 
